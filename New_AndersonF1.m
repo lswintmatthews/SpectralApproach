c0b49840da6e595ede0a68ce8b14fa96ede8396a
jlpadgett <joshua.padgett@ttu.edu>
2019-02-16 18:28:34 -0600

    Update of spectral codes. New_AndersonF1 fixes a bug in the paralle calculation. New_AndersonF2 computes s=1 explicitly (rather than via an averaging).

---
+++ New_AndersonF1.m
@@ -0,0 +1,359 @@
+function [YY,Z] = New_AndersonF1(ss,n,c,influence,P,trials)
+
+%
+% Pre:
+%   s:              vector containing values for desired fractional 
+%                   exponents
+%   n:              number of time steps (n-2 is actual number of steps)
+%   c:              c-value for random parameter interval length
+%   influence:      how many neighbor interactions to allow (note that if
+%                   this parameter is left empty the function will choose
+%                   100 by default)
+%   P:              chooses the particular delta vector (options are 1, 2,
+%                   and 3, with 3 being the "widest"
+%   trials:         number of trials to perform for averaging results
+%
+% Post:             
+%   YY:             Stores the average (over all trials) of each s-value
+%                   in a particular column (increasing left to right)
+%   Z:              Stores each indiviual result for deeper analysis (it
+%                   will store the s-values for each trial from left to 
+%                   right in a multi-dimensional array)
+%
+% Note that this functions saves the data in particular directories
+%
+
+if nargin <= 3
+    influence = 200;
+end
+
+if nargin <=4
+    P = 3; % Chooses the "widest" vector by default
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                                                                      %%
+%% Code for one-dimensional Anderson localization simulations via the   %%
+%% spectral approach.                                                   %%
+%%                                                                      %%
+%% This code allows for the consideration of fractional diffusion for   %%
+%% all fractions 0 < s < 2. The code employs different methods based on %%
+%% the choise of s.                                                     %%
+%%                                                                      %%
+%% All methods herein are based on the Bessel-type approach.            %%
+%%                                                                      %%
+%% All necessary parameters that may be changed are outlined below.     %%
+%%                                                                      %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Start of Code                                                        %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+close all
+clc
+rng('Shuffle')
+
+max = length(ss); % Determines the number of s-values to run
+YY = zeros(n-2,max); % Empty vector for storing final result
+Z = zeros(n-2,max,trials); % Empty vector for storing individual results
+
+%% The following "parfor" loop while parallelize the process of
+%% running multiple trials
+
+parfor iii = 1:trials
+    
+    %% Initialize relevant vector and values
+    
+    N = 2*influence*n+1; % Lattice size
+    Y = zeros(n-2,max);  % Solution vector for given trial
+    Omega = c.*(rand(N,1)-0.5*ones(N,1)); % Vector of random values
+    NN = 0.5*(N-1)+1; % Midpoint of lattice (for starting point)
+    
+    %% The following loops runs computations for each s-value
+    
+    for ll = 1:max
+    
+        s = ss(ll); % Extracts s-value from appropriate position
+    
+        if s ~= 1 % Checks s-value
+    
+            A = zeros(N,3); % Placeholder vector for the simulation
+            A(NN,1) = 1; % Builds delta_0
+    
+            % Weights for computing action of fractional Laplacian
+            
+            ks = (4^s)*gamma(0.5+s)/((sqrt(pi))*abs(gamma(-s))); % Constant
+            K_s = zeros(influence-1,1); % Vector to store computed weights
+            kg = gamma(1-s)/gamma(2+s); 
+            for j=1:(influence-1)
+                K_s(j) = ks*kg; 
+                kg = ((j-s)/(j+1-s))*kg; 
+            end
+            
+            p = 0; % Initializes distance calculation
+            
+            % Loop to compute first action
+    
+            for k=NN-influence:NN+influence
+                
+                f1 = 0; % Placeholder
+                
+                for j=1:(influence-1)
+                    
+                    % Primary action of fractional Laplacian at node
+                    
+                    f1 = f1 + (2*A(k,1)-A(k-j,1)-A(k+j,1))*K_s(j);
+                    
+                end     
+                
+                A(k,2) = f1; % Stores final value
+                
+            end
+            
+            A(:,2) = A(:,2)./norm(A(:,2)); % Normalizes vector
+    
+            % Loop to compute remaining actions
+            
+            for i=2:(n-1)
+                
+                % Inner loop computes each new "action"
+                
+                for k=NN-influence*i:NN+influence*i
+                    
+                f1 = 0; % Placeholder
+                
+                    for j=1:(influence-1)
+                        
+                        % Primary action of fractional Laplacian at node
+                        
+                        f1 = f1 + (2*A(k,2)-A(k-j,2)-A(k+j,2))*K_s(j);
+                        
+                    end  
+                    
+                    A(k,3) = f1;
+                    
+                end
+                
+                A(:,3) = A(:,3) + A(:,2).*Omega; % Adds randomness
+                
+                % The following two lines perform Gram-Schmidt
+        
+                A(:,3) = A(:,3) - ((A(:,3))'*A(:,2)).*A(:,2) - ((A(:,3))'*A(:,1)).*A(:,1);
+                A(:,3) = A(:,3)./norm(A(:,3));
+                
+                % The next set of code computes the distance value based
+                % on the P-value submitted
+                
+                if P == 1
+                    
+                    p = p + (A(NN-1,3))^2;
+                    
+                elseif P == 2
+                    
+                    p = p + 0.25*((A(NN-1,3)-A(NN+1,3)+A(NN-2,3)-A(NN+2,3))^2);
+                    
+                elseif P == 3
+                    
+                    pp = 0;
+                        for ii = 1:influence-1
+                            pp = pp + ((-1)^(ii-1))*(A(NN-ii,3)) + ((-1)^(ii))*(A(NN+ii,3));
+                        end
+                    p = p + 0.5*(1/((influence-1)))*(pp^2);
+                    
+                end
+                
+                % Stops computations when "localization" occurs
+                
+                if p >= 1
+                    disp(sprintf('Numerical localization has occurred after %1.0f iterations. Possibly vary the influence.',i))
+                    break
+                end
+        
+                Y(i-1,ll) = sqrt(1-p); % Stores computed distance
+
+                A(:,1) = A(:,2); % Updates vector
+                A(:,2) = A(:,3); % Updates vector
+                
+            end
+    
+        end
+        
+        % Due to the sensitivity of this code, we will approximate the s=1
+        % case via a simple averaging
+        
+        if s == 1
+            
+            s1 = 0.999999999999; % Helps with averaging
+            s2 = 1.000000000001; % Helps with averaging
+            
+            for mm = 1:2
+                
+                % Chooses s-value for averaging
+                if mm == 1
+                    s = s1;
+                elseif mm == 2
+                    s = s2;
+                end
+                
+                W = zeros(n-2,2); % Temporary storage for the averaging
+    
+                A = zeros(N,3); % Placeholder vector for simulation
+                A(NN,1) = 1; % Builds delta_0
+                
+                % Weights for computing action of fractional Laplacian
+            
+                ks = (4^s)*gamma(0.5+s)/((sqrt(pi))*abs(gamma(-s))); % Constant
+                K_s = zeros(influence-1,1); % Vector to store computed weights
+                kg = gamma(1-s)/gamma(2+s); 
+                for j=1:(influence-1)
+                    K_s(j) = ks*kg; 
+                    kg = ((j-s)/(j+1-s))*kg; 
+                end
+            
+                p = 0; % Initializes distance calculation
+            
+                % Loop to compute first action
+    
+                for k=NN-influence:NN+influence
+                
+                    f1 = 0; % Placeholder
+                
+                    for j=1:(influence-1)
+                    
+                        % Primary action of fractional Laplacian at node
+                    
+                        f1 = f1 + (2*A(k,1)-A(k-j,1)-A(k+j,1))*K_s(j);
+                    
+                    end     
+                
+                    A(k,2) = f1; % Stores final value
+                
+                end
+            
+                A(:,2) = A(:,2)./norm(A(:,2)); % Normalizes vector
+    
+                % Loop to compute remaining actions
+            
+                for i=2:(n-1)
+                
+                    % Inner loop computes each new "action"
+                
+                    for k=NN-influence*i:NN+influence*i
+                    
+                    f1 = 0; % Placeholder
+                
+                        for j=1:(influence-1)
+                        
+                            % Primary action of fractional Laplacian at node
+                        
+                            f1 = f1 + (2*A(k,2)-A(k-j,2)-A(k+j,2))*K_s(j);
+                        
+                        end  
+                    
+                        A(k,3) = f1;
+                    
+                    end
+                
+                    A(:,3) = A(:,3) + A(:,2).*Omega; % Adds randomness
+                
+                    % The following two lines perform Gram-Schmidt
+        
+                    A(:,3) = A(:,3) - ((A(:,3))'*A(:,2)).*A(:,2) - ((A(:,3))'*A(:,1)).*A(:,1);
+                    A(:,3) = A(:,3)./norm(A(:,3));
+                
+                    % The next set of code computes the distance value based
+                    % on the P-value submitted
+                
+                    if P == 1
+                    
+                        p = p + (A(NN-1,3))^2;
+                    
+                    elseif P == 2
+                    
+                        p = p + 0.25*((A(NN-1,3)-A(NN+1,3)+A(NN-2,3)-A(NN+2,3))^2);
+                    
+                    elseif P == 3
+                    
+                        pp = 0;
+                            for ii = 1:influence-1
+                                pp = pp + ((-1)^(ii-1))*(A(NN-ii,3)) + ((-1)^(ii))*(A(NN+ii,3));
+                            end
+                        p = p + 0.5*(1/((influence-1)))*(pp^2);
+                    
+                    end
+                
+                    % Stops computations when "localization" occurs
+                
+                    if p >= 1
+                        disp(sprintf('Numerical localization has occurred after %1.0f iterations. Possibly vary the influence.',i))
+                        break
+                    end
+        
+                    W(i-1,mm) = sqrt(1-p); % Stores computed distance
+
+                    A(:,1) = A(:,2); % Updates vector
+                    A(:,2) = A(:,3); % Updates vector
+                    
+                end
+                
+                Y(:,ll) = Y(:,ll) + W(:,mm); % Aids in averaging
+    
+            end     
+        
+            Y(:,ll) = 0.5*Y(:,ll); % Computes the average of the runs
+            
+        end
+    
+    end
+    
+    Z(:,:,iii) = Y; % Stores all of the data
+    
+    YY = YY + Y; % Aids in storing averages
+    
+end
+
+YY = (1/trials)*YY; % Stores the average of the runs
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Save Data                                                            %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Creates string for c value with appropriate length
+
+s = ss;
+
+cc = floor(c);
+cString1 = num2str(cc);
+lc1 = length(cString1);
+ccc = c - cc;
+cString2 = num2str(ccc);
+lc2 = length(cString2);
+cString = [cString1 '_' cString2(lc1+2:lc2)];
+
+%% Making Directories
+
+if exist('Fractional_Anderson_Data', 'dir') == 0 
+    mkdir('Fractional_Anderson_Data');
+else
+    warning('off','MATLAB:MKDIR:DirectoryExists');
+end
+if exist(['Fractional_Anderson_Data_s_' num2str(s(1)) '-' num2str(s(max))], 'dir') == 0 
+    mkdir('Fractional_Anderson_Data',['Fractional_Anderson_Data_s_' num2str(s(1)) '-' num2str(s(max))]);
+else
+    warning('off','MATLAB:MKDIR:DirectoryExists');
+end
+ddir = fullfile('Fractional_Anderson_Data',['Fractional_Anderson_Data_s_' num2str(s(1)) '-' num2str(s(max))]);
+if exist(['Fractional_Anderson_Data_s_' num2str(s(1)) '-' num2str(s(max)) '_c_' cString], 'dir') == 0
+    mkdir(ddir,['Fractional_Anderson_Data_s_' num2str(s(1)) '-' num2str(s(max)) '_c_' cString]);
+else
+    warning('off','MATLAB:MKDIR:DirectoryExists');
+end
+
+%% Saving Data
+
+FolderDestination = ['Fractional_Anderson_Data_s_' num2str(s(1)) '-' num2str(s(max)) '_c_' cString];
+data = ['Frac_Data_s_' num2str(s(1)) '-' num2str(s(max)) '_c_' cString '_n_' num2str(n) '_infl_' num2str(influence) '_trials_' num2str(trials) '.mat'];
+matfile = fullfile(ddir,FolderDestination,data);
+
+save(matfile);
